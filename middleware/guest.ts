3c09df310ca2dce4c62b741c93574dd74ea41ae3
Asad Ullah <assadudev@gmail.com>
2024-10-16 14:54:22 +0500

    guest middleware

---
+++ middleware/guest.ts
@@ -0,0 +1,29 @@
+// middleware/guest.ts
+import { useAuthStore } from '~/stores/auth';
+
+export default defineNuxtRouteMiddleware(async (to, from) => {
+  const authStore = useAuthStore();
+  console.log("guest",to)
+  console.log("guest",from)
+
+  // Ensure this code runs only in the client
+  if (process.client) {
+    await authStore.initialize(); // Initialize auth state from localStorage or API
+  }
+
+  const isLoggedIn = authStore.isAuthenticated(); // Check if the user is authenticated
+
+  // Redirect authenticated users to the dashboard if they try to access login
+  if (isLoggedIn) {
+    if (to.path === '/login') {
+      return navigateTo('/dashboard'); // Redirect to dashboard if already logged in
+    }
+  } else {
+    // If not authenticated, allow access to login page but redirect if accessing other pages
+    if (to.path !== '/login') {
+      return navigateTo('/login'); // Redirect to login if not logged in and trying to access a different page
+    }
+  }
+
+  // Allow access to the login page for guest users
+});
