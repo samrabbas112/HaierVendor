a77fd8a6b5b8c8b13672fe2f5535e4eecaac1747
Asad Ullah <assadudev@gmail.com>
2024-10-18 11:39:51 +0500

    create page for management and activity and add navigation

--- navigation/vertical/index.ts
+++ navigation/vertical/index.ts
@@ -1,8 +1,8 @@
 export default [
   {
-    title: "Dashboards",
-    to: { name: "dashboard" },
-    icon: { icon: "tabler-smart-home" },
+    title: 'Dashboards',
+    to: { name: 'dashboard' },
+    icon: { icon: 'tabler-smart-home' },
   },
   {
     title: 'Orders',
@@ -10,13 +10,41 @@ export default [
     children: [
       {
         title: 'Haier Orders',
-        to: { name: 'order-list-haier'},
+        to: { name: 'order-list-haier' },
       },
       {
         title: 'Vendor Orders',
-        to: { name: 'order-list'},
+        to: { name: 'order-list' },
       },
     ],
   },
+  {
+    title: 'Management',
+    icon: { icon: 'tabler-shopping-cart' },
+    children: [
+      {
+        title: 'Vendors',
+        to: { name: 'management-vendors' },
+      },
+      {
+        title: 'Customers',
+        to: { name: 'management-customers' },
+      },
+      {
+        title: 'Queries',
+        to: { name: 'management-queries' },
+      },
+    ],
+  },
+  {
+    title: 'Activity',
+    icon: { icon: 'tabler-activity' },
+    children: [
+      {
+        title: 'cashback',
+        to: { name: 'activity-cashback' },
+      }
+    ]
+  }
 
-];
+]
