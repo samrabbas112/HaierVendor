a210a64a341378c1d8d577d239c23e1288390c57
samrabbas112 <samrabbas112@gmail.com>
2024-11-28 10:30:42 +0500

    Customer Apis

--- pages/management/customers.vue
+++ pages/management/customers.vue
@@ -1,108 +1,20 @@
 <script setup lang="ts">
-import mockUsers from './mock-json/mock-users.json'
-// Adjust the path as necessary
-import AddNewUserDrawer from '@/views/user/list/AddNewUserDrawer.vue'
-import type { UserProperties } from '@db/apps/users/types'
+import AddNewCustomerDrawer from '@/views/customer/AddNewCustomerDrawer.vue'
+import { ref, onMounted, watch, nextTick } from 'vue';
 
-// 👉 Store
-const searchQuery = ref('')
-const selectedRole = ref()
-const selectedPlan = ref()
-const selectedStatus = ref()
-// const usersData = ref();
+// State
+const searchQuery = ref('');
+const selectedCustomer = ref(null);
+const customersData = ref({ total: 0, customers: [] });
+const isAddNewCustomerDrawerVisible = ref(false);
+const selectedRows = ref([]);
 
 // Data table options
-const itemsPerPage = ref(10)
-const page = ref(1)
-const sortBy = ref()
-const orderBy = ref()
-const selectedRows = ref([])
+const itemsPerPage = ref(10); // Set a default items per page (defaulting to 10)
+const page = ref(1);
+const apiRequestObj = useApi();
 
-// Update data table options
-const updateOptions = (options: any) => {
-  sortBy.value = options.sortBy[0]?.key
-  orderBy.value = options.sortBy[0]?.order
-}
-
-// Headers
-const headers = [
-  { title: 'SR', key: 'sr' },
-  { title: 'Vendor ID', key: 'vendor-id' },
-  { title: 'Vendor Detail', key: 'vendor-detail' },
-  { title: 'Status', key: 'status' },
-  { title: 'Creation Time', key: 'create-time' },
-  { title: 'Actions', key: 'actions', sortable: false },
-]
-
-// Async function to fetch mock data
-async function fetchMockUsers() {
-  return mockUsers
-}
-
-const { data: usersData } = await fetchMockUsers()
-
-// Log the fetched data
-if (usersData)
-  console.log('Fetched Users Data:', usersData)
-else
-  console.error('No data fetched.')
-
-// 👉 Fetch users on component mount
-onMounted(async () => {
-  const fetchedData = await fetchMockUsers();
-  if (fetchedData?.data) {
-    usersData.value = fetchedData.data; // Populate the `usersData` array
-    totalUsers.value = fetchedData.data.length; // Calculate total users
-    console.log('Fetched Users Data:', usersData.value);
-  } else {
-    console.error('No data fetched.');
-  }
-});
-
-// Computed properties
-const users = computed(() => usersData.value || []); // Reactive `users` array
-
-console.log('users: ', usersData);
-console.log('totalUsers: ', usersData.length)
-// 👉 search filters
-const roles = [
-  { title: 'Admin', value: 'admin' },
-  { title: 'Author', value: 'author' },
-  { title: 'Editor', value: 'editor' },
-  { title: 'Maintainer', value: 'maintainer' },
-  { title: 'Subscriber', value: 'subscriber' },
-]
-
-const plans = [
-  { title: 'Basic', value: 'basic' },
-  { title: 'Company', value: 'company' },
-  { title: 'Enterprise', value: 'enterprise' },
-  { title: 'Team', value: 'team' },
-]
-
-const status = [
-  { title: 'Pending', value: 'pending' },
-  { title: 'Active', value: 'active' },
-  { title: 'Inactive', value: 'inactive' },
-]
-
-const resolveUserRoleVariant = (role: string) => {
-  const roleLowerCase = role.toLowerCase()
-
-  if (roleLowerCase === 'subscriber')
-    return { color: 'success', icon: 'tabler-user' }
-  if (roleLowerCase === 'author')
-    return { color: 'error', icon: 'tabler-device-desktop' }
-  if (roleLowerCase === 'maintainer')
-    return { color: 'info', icon: 'tabler-chart-pie' }
-  if (roleLowerCase === 'editor')
-    return { color: 'warning', icon: 'tabler-edit' }
-  if (roleLowerCase === 'admin')
-    return { color: 'primary', icon: 'tabler-crown' }
-
-  return { color: 'primary', icon: 'tabler-user' }
-}
-
+// Resolve user status for the UI
 const resolveUserStatusVariant = (stat: string) => {
   const statLowerCase = stat.toLowerCase()
   if (statLowerCase === 'pending')
@@ -115,324 +27,156 @@ const resolveUserStatusVariant = (stat: string) => {
   return 'primary'
 }
 
-const isAddNewUserDrawerVisible = ref(false)
+// Headers for the data table
+const headers = [
+  { title: 'ID', key: 'id' },
+  { title: 'Name', key: 'name' },
+  { title: 'Phone', key: 'phone_number' },
+  { title: 'City', key: 'city' },
+  { title: 'Status', key: 'confirmed' },
+  { title: 'Registered At', key: 'created_at' },
+  { title: 'Actions', key: 'actions', sortable: false },
+];
 
-// 👉 Add new user
-const addNewUser = async (userData: UserProperties) => {
-  await $api('/apps/users', {
-    method: 'POST',
-    body: userData,
-  })
+// Fetch customers from the API
+const fetchCustomers = async () => {
+  try {
+    console.log('sam');
+    console.log(page.value);
+    const response = await apiRequestObj.makeRequest('common/customer/list', 'get', {
+      params: {
+        page: page.value,
+        itemsPerPage: itemsPerPage.value,
+        query: searchQuery.value,
+      },
+    });
 
-  // Refetch User
-  fetchUsers()
-}
+    if (response?.success) {
+      customersData.value = {
+        total: response.data.total,
+        customers: response.data.customers,
+      };
+    }
+  } catch (error) {
+    console.error('Error fetching customers:', error);
+  }
+};
 
-// 👉 Delete user
-const deleteUser = async (id: number) => {
-  await $api(`/apps/users/${id}`, {
-    method: 'DELETE',
-  })
 
-  // Delete from selectedRows
-  const index = selectedRows.value.findIndex(row => row === id)
-  if (index !== -1)
-    selectedRows.value.splice(index, 1)
+// Delete a customer
+const deleteCustomer = async (id) => {
+  try {
+    const response = await apiRequestObj.makeRequest(`common/customer/delete/${id}`, 'DELETE');
+    if (response?.success) {
+      fetchCustomers();
+    }
+  } catch (error) {
+    console.error('Error deleting customer:', error);
+  }
+};
 
-  // refetch User
-  // TODO: Make this async
-  fetchUsers()
-}
+// Handle Add New Customer
+const addNewCustomer = () => {
+  selectedCustomer.value = null;
+  isAddNewCustomerDrawerVisible.value = true;
+};
 
-const widgetData = ref([
-  { title: 'Total Customers', value: '21', change: 29, desc: 'Total Vendor', icon: 'tabler-users', iconColor: 'primary' },
-  { title: 'Active Customers', value: '19', change: -14, desc: 'Total Active Vendor', icon: 'tabler-user-check', iconColor: 'success' },
-  { title: 'InActive Customers', value: '2', change: 42, desc: 'Last Week Analytics', icon: 'tabler-user-search', iconColor: 'error' },
-])
+// Handle Edit Customer
+const editCustomer = (customer) => {
+  selectedCustomer.value = { ...customer };
+  isAddNewCustomerDrawerVisible.value = true;
+};
+
+// Watch for changes in filters or pagination
+watch(page, (newPage) => {
+  console.log('Current Page:', newPage);
+  fetchCustomers();  
+});
+const handleCustomerUpdated = () => {
+  fetchCustomers(); 
+};
+
+onMounted(fetchCustomers);
 </script>
+
 
 <template>
   <section>
-    <!-- 👉 Widgets -->
-    <div class="d-flex mb-6">
-      <VRow>
-        <template
-          v-for="(data, id) in widgetData"
-          :key="id"
-        >
-          <VCol
-            cols="12"
-            md="3"
-            sm="6"
-          >
-            <VCard>
-              <VCardText>
-                <div class="d-flex justify-space-between">
-                  <div class="d-flex flex-column gap-y-1">
-                    <div class="text-body-1 text-high-emphasis">
-                      {{ data.title }}
-                    </div>
-                    <div class="d-flex gap-x-2 align-center">
-                      <h4 class="text-h4">
-                        {{ data.value }}
-                      </h4>
-                      <div
-                        class="text-base"
-                        :class="data.change > 0 ? 'text-success' : 'text-error'"
-                      >
-                        ({{ prefixWithPlus(data.change) }}%)
-                      </div>
-                    </div>
-                    <div class="text-sm">
-                      {{ data.desc }}
-                    </div>
-                  </div>
-                  <VAvatar
-                    :color="data.iconColor"
-                    variant="tonal"
-                    rounded
-                    size="42"
-                  >
-                    <VIcon
-                      :icon="data.icon"
-                      size="26"
-                    />
-                  </VAvatar>
-                </div>
-              </VCardText>
-            </VCard>
-          </VCol>
-        </template>
-      </VRow>
-    </div>
-
+    <!-- Customer Filters -->
     <VCard class="mb-6">
-      <VCardItem class="pb-4">
-        <VCardTitle>Filters</VCardTitle>
-      </VCardItem>
-
+      <VCardTitle>Filters</VCardTitle>
       <VCardText>
         <VRow>
-          <!-- 👉 Select Role -->
-          <VCol
-            cols="12"
-            sm="4"
-          >
-            <AppSelect
-              v-model="selectedRole"
-              placeholder="Select Role"
-              :items="roles"
-              clearable
-              clear-icon="tabler-x"
-            />
+          <!-- Search Input -->
+          <VCol cols="12" sm="4">
+            <AppTextField v-model="searchQuery" placeholder="Search Customers" />
           </VCol>
-          <!-- 👉 Select Plan -->
-          <VCol
-            cols="12"
-            sm="4"
-          >
+          <!-- Items Per Page -->
+          <VCol cols="12" sm="4">
             <AppSelect
-              v-model="selectedPlan"
-              placeholder="Select Plan"
-              :items="plans"
-              clearable
-              clear-icon="tabler-x"
-            />
-          </VCol>
-          <!-- 👉 Select Status -->
-          <VCol
-            cols="12"
-            sm="4"
-          >
-            <AppSelect
-              v-model="selectedStatus"
-              placeholder="Select Status"
-              :items="status"
-              clearable
-              clear-icon="tabler-x"
+              v-model="itemsPerPage"
+              :items="[10, 25, 50, 100]"
+              label="Items Per Page"
             />
           </VCol>
         </VRow>
       </VCardText>
-
-      <VDivider />
-
-      <VCardText class="d-flex flex-wrap gap-4">
-        <div class="me-3 d-flex gap-3">
-          <AppSelect
-            :model-value="itemsPerPage"
-            :items="[
-              { value: 10, title: '10' },
-              { value: 25, title: '25' },
-              { value: 50, title: '50' },
-              { value: 100, title: '100' },
-              { value: -1, title: 'All' },
-            ]"
-            style="inline-size: 6.25rem;"
-            @update:model-value="itemsPerPage = parseInt($event, 10)"
-          />
-        </div>
-        <VSpacer />
-
-        <div class="app-user-search-filter d-flex align-center flex-wrap gap-4">
-          <!-- 👉 Search  -->
-          <div style="inline-size: 15.625rem;">
-            <AppTextField
-              v-model="searchQuery"
-              placeholder="Search User"
-            />
-          </div>
-
-
-
-          <!-- 👉 Add user button -->
-          <VBtn
-            prepend-icon="tabler-plus"
-            @click="isAddNewUserDrawerVisible = true"
-          >
-            Add New Customer
-          </VBtn>
-        </div>
-      </VCardText>
-
-      <VDivider />
-
-      <!-- SECTION datatable -->
-      <VDataTableServer
-        v-model:items-per-page="itemsPerPage"
-        v-model:model-value="selectedRows"
-        v-model:page="page"
-        :items="users"
-        item-value="id"
-        :items-length="totalUsers"
-        :headers="headers"
-        class="text-no-wrap"
-        show-select
-        @update:options="updateOptions"
-      >
-        <!-- User -->
-        <template #item.user="{ item }">
-          <div class="d-flex align-center gap-x-4">
-            <VAvatar
-              size="34"
-              :variant="!item.avatar ? 'tonal' : undefined"
-              :color="!item.avatar ? resolveUserRoleVariant(item.role).color : undefined"
-            >
-              <VImg
-                v-if="item.avatar"
-                :src="item.avatar"
-              />
-              <span v-else>{{ avatarText(item.fullName) }}</span>
-            </VAvatar>
-            <div class="d-flex flex-column">
-              <h6 class="text-base">
-                <NuxtLink
-                  :to="{ name: 'apps-user-view-id', params: { id: item.id } }"
-                  class="font-weight-medium text-link"
-                >
-                  {{ item.fullName }}
-                </NuxtLink>
-              </h6>
-              <div class="text-sm">
-                {{ item.email }}
-              </div>
-            </div>
-          </div>
-        </template>
-
-        <!-- 👉 Role -->
-        <template #item.role="{ item }">
-          <div class="d-flex align-center gap-x-2">
-            <VIcon
-              :size="22"
-              :icon="resolveUserRoleVariant(item.role).icon"
-              :color="resolveUserRoleVariant(item.role).color"
-            />
-
-            <div class="text-capitalize text-high-emphasis text-body-1">
-              {{ item.role }}
-            </div>
-          </div>
-        </template>
-
-        <!-- Plan -->
-        <template #item.plan="{ item }">
-          <div class="text-body-1 text-high-emphasis text-capitalize">
-            {{ item.currentPlan }}
-          </div>
-        </template>
-
-        <!-- Status -->
-        <template #item.status="{ item }">
-          <VChip
-            :color="resolveUserStatusVariant(item.status)"
-            size="small"
-            label
-            class="text-capitalize"
-          >
-            {{ item.status }}
-          </VChip>
-        </template>
-
-        <!-- Actions -->
-        <template #item.actions="{ item }">
-          <IconBtn @click="deleteUser(item.id)">
-            <VIcon icon="tabler-trash" />
-          </IconBtn>
-
-          <IconBtn>
-            <VIcon icon="tabler-eye" />
-          </IconBtn>
-
-          <VBtn
-            icon
-            variant="text"
-            color="medium-emphasis"
-          >
-            <VIcon icon="tabler-dots-vertical" />
-            <VMenu activator="parent">
-              <VList>
-                <VListItem :to="{ name: 'apps-user-view-id', params: { id: item.id } }">
-                  <template #prepend>
-                    <VIcon icon="tabler-eye" />
-                  </template>
-
-                  <VListItemTitle>View</VListItemTitle>
-                </VListItem>
-
-                <VListItem link>
-                  <template #prepend>
-                    <VIcon icon="tabler-pencil" />
-                  </template>
-                  <VListItemTitle>Edit</VListItemTitle>
-                </VListItem>
-
-                <VListItem @click="deleteUser(item.id)">
-                  <template #prepend>
-                    <VIcon icon="tabler-trash" />
-                  </template>
-                  <VListItemTitle>Delete</VListItemTitle>
-                </VListItem>
-              </VList>
-            </VMenu>
-          </VBtn>
-        </template>
-
-        <!-- pagination -->
-        <template #bottom>
-          <TablePagination
-            v-model:page="page"
-            :items-per-page="itemsPerPage"
-            :total-items="totalUsers"
-          />
-        </template>
-      </VDataTableServer>
-      <!-- SECTION -->
     </VCard>
-    <!-- 👉 Add New User -->
-    <AddNewUserDrawer
-      v-model:isDrawerOpen="isAddNewUserDrawerVisible"
-      @user-data="addNewUser"
+
+    <!-- Customers Table -->
+    <VDataTableServer 
+      v-model:items-per-page="itemsPerPage" 
+      v-model:model-value="selectedRows" 
+      v-model:page="page"
+      :items="customersData.customers" 
+      item-value="id" 
+      :items-length="customersData.total" 
+      :headers="headers"
+      class="text-no-wrap" 
+      show-select 
+      @update:options="updateOptions"
+    >
+      <template #item.id="{ item }">
+        <NuxtLink>
+          #{{ item.id }}
+        </NuxtLink>
+      </template>
+      <template #item.created_at="{ item }">
+        {{ new Date(item.created_at).toDateString() }}
+      </template>
+      <!-- Status -->
+      <template #item.confirmed="{ item }">
+        <VChip :color="resolveUserStatusVariant(item.confirmed)" size="small" label class="text-capitalize">
+          {{ item.confirmed == 1 ? 'Active' : 'InActive' }}
+        </VChip>
+      </template>
+
+      <!-- Actions -->
+      <template #item.actions="{ item }">
+        <!-- Trash Icon Button -->
+        <IconBtn @click="deleteCustomer(item.uid)">
+          <VIcon icon="tabler-trash" />
+        </IconBtn>
+
+        <IconBtn @click="editCustomer(item)">
+          <VIcon icon="tabler-pencil" />
+        </IconBtn>
+      </template>
+
+      <!-- pagination -->
+      <template #bottom>
+        <TablePagination v-model:page="page" :items-per-page="itemsPerPage" :total-items="customersData.total" />
+      </template>
+    </VDataTableServer>
+
+    <!-- Add Customer Button -->
+    <VBtn icon="tabler-plus" @click="addNewCustomer">Add New Customer</VBtn>
+
+    <!-- Add/Edit Drawer -->
+    <AddNewCustomerDrawer
+      v-model:isDrawerOpen="isAddNewCustomerDrawerVisible"
+      :customer="selectedCustomer"
+      @customer-updated="handleCustomerUpdated"
     />
   </section>
 </template>
