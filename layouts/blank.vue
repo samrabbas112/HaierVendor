6dbede600dca1bfb5bac45cdf358638458a11ee8
= <=>
2024-12-26 18:16:31 +0500

    alert issue resovleD

--- layouts/blank.vue
+++ layouts/blank.vue
@@ -1,5 +1,5 @@
 <script lang="ts" setup>
-import Alert from "@/components/alert/alert.vue";
+import Alert from "@/components/alert/Alert.vue";
 import Loader from "@/components/loader/Loader.vue";
 
 const { injectSkinClasses } = useSkins();
