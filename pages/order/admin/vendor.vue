60be4fbf7ec2a3da2fbc6a0eb699a99ad5252d2d
ahmad841pk <contact.mahmad1@gmail.com>
2025-01-06 12:35:26 +0500

    change status to order status

--- pages/order/admin/vendor.vue
+++ pages/order/admin/vendor.vue
@@ -33,7 +33,7 @@ const headers = [
 
   // { title: "Placed At", key: "date", sortable: false },
   { title: 'Assigned To', key: 'vendors', sortable: false },
-  { title: 'Status', key: 'status', sortable: false },
+  { title: 'Order Status', key: 'status', sortable: false },
   { title: 'Action', key: 'actions', sortable: false },
 ]
 
