d2f6ca1bd1a6a8d69568c978990ba9d5e08db8f7
samrabbas112 <samrabbas112@gmail.com>
2025-01-22 17:37:24 +0500

    search in cities

--- @core/components/app-form-elements/AppSelect.vue
+++ @core/components/app-form-elements/AppSelect.vue
@@ -18,33 +18,27 @@ const label = computed(() => useAttrs().label as string | undefined);
 
 <template>
   <div class="app-select flex-grow-1" :class="$attrs.class">
-    <VLabel
-      v-if="label"
-      :for="elementId"
-      class="mb-1 text-body-2"
-      style="line-height: 15px"
-      :text="label"
-    />
-    <VSelect
-      v-bind="{
-        ...$attrs,
-        class: null,
-        label: undefined,
-        variant: 'outlined',
-        id: elementId,
-        menuProps: {
-          contentClass: [
-            'app-inner-list',
-            'app-select__content',
-            'v-select__content',
-            $attrs.multiple !== undefined ? 'v-list-select-multiple' : '',
-          ],
-        },
-      }"
-    >
+    <VLabel v-if="label" :for="elementId" class="mb-1 text-body-2" style="line-height: 15px" :text="label" />
+    <VSelect v-bind="{
+      ...$attrs,
+      class: null,
+      label: undefined,
+      variant: 'outlined',
+      id: elementId,
+      menuProps: {
+        contentClass: [
+          'app-inner-list',
+          'app-select__content',
+          'v-select__content',
+          $attrs.multiple !== undefined ? 'v-list-select-multiple' : '',
+        ],
+      },
+      filterable: true // Removed the extra comma here
+    }">
       <template v-for="(_, name) in $slots" #[name]="slotProps">
         <slot :name="name" v-bind="slotProps || {}" />
       </template>
     </VSelect>
+
   </div>
 </template>
